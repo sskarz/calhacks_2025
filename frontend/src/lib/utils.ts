98ddae7c2fceb01db00a545ffc01b712a6fe6c00
Sanskar Thapa <sanskar1016pro@outlook.com>
2025-10-25 02:20:58 -0700

    removed old frontend code, replaced with tetsy

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,15 +1,6 @@
-export const formatCurrency = (value: number, currency = "USD") =>
-  new Intl.NumberFormat("en-US", { style: "currency", currency }).format(value);
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export const formatRelativeDate = (iso: string) => {
-  const date = new Date(iso);
-  return new Intl.DateTimeFormat("en-US", {
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric"
-  }).format(date);
-};
-
-export const generateId = () =>
-  (crypto?.randomUUID ? crypto.randomUUID() : Math.random().toString(36).slice(2));
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
